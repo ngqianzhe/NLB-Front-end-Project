af5a4bd1170e81a76f9ed33e7ca53639e6b3c163
Ng Qian Zhe <s10243009@connect.np.edu.sg>
2024-12-22 16:53:07 +0800

    edited front-end and packages installed

--- src/footer.jsx
+++ src/footer.jsx
@@ -10,7 +10,7 @@ function footer() {
       <div className="footer">
       <div className="footer-content">
         <ul className="left-links">
-          <li><a href="https://www.tech.gov.sg/report-vulnerability/" className="list-text">Report Vulnerability  <FontAwesomeIcon icon={faRightFromBracket} size="sm" color="grey"/></a></li>
+          <li><a href="https://www.tech.gov.sg/report-vulnerability/" className="list-text">Report Vulnerability  <FontAwesomeIcon icon={faRightFromBracket} size="sm" color="rgb(174, 172, 172)"/></a></li>
           <li><a href="https://www.nlb.gov.sg/main/Privacy-Statement" className="list-text">Privacy Statement</a></li>
           <li><a href="https://www.nlb.gov.sg/main/Terms-of-Use" className="list-text">Terms of Use</a></li>
           <li><a href="https://www.tech.gov.sg/terms-of-use/" className="list-text">Other Policies</a></li>
