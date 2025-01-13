71c076bb324d97e32a811bf6be8c96ba53f552da
Ng Qian Zhe <s10243009@connect.np.edu.sg>
2025-01-14 00:49:48 +0800

    Edited NLB API

--- src/Home.jsx
+++ src/Home.jsx
@@ -336,14 +336,14 @@ const Home = () => {
   useEffect(() => {
     const fetchOpeningHours = async () => {
       setIsLoading(true);
+      const myHeaders = new Headers();
+      myHeaders.append("x-api-key", "<P}o$s$v.|X4~w#*4/+8GxrIJd_I5Wtt");
+      myHeaders.append("x-app-code", "DEV-NgQianZhe");
+      myHeaders.append("Cookie", "AWSALB=ocEaQ9sXcmDiQFk2OFXbzDQDGfDQ6evQAvAjS2/l+mJd0Z/fdfEq4/JjVRGAoJnCGpRyrRoXNR/lM8MV/pFXxSsQBSgmCuU6MfzZP97ZQ/FrLqQBlU65H4my3l75; AWSALBCORS=ocEaQ9sXcmDiQFk2OFXbzDQDGfDQ6evQAvAjS2/l+mJd0Z/fdfEq4/JjVRGAoJnCGpRyrRoXNR/lM8MV/pFXxSsQBSgmCuU6MfzZP97ZQ/FrLqQBlU65H4my3l75");
       try {
-        
-        const apiUrl = `api/v1/Library/GetBranches`;
+        const apiUrl = 'https://openweb.nlb.gov.sg/api/v1/Library/GetBranches';
         const response = await fetch(apiUrl, {
-          headers: {
-            'x-api-key': '<P}o$s$v.|X4~w#*4/+8GxrIJd_I5Wtt',
-            'x-app-code': 'DEV-NgQianZhe'
-          }
+          headers: myHeaders
         });
 
         const data = await response.json();
